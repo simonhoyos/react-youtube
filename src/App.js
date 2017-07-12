5c8ee173a40ac6210642a0feb658a37b8cc20977
shmesa22 <shmesa23@gmail.com>
2017-07-12 15:26:24 -0500

    Implement functionality see video details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import YTSSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBkYZoFpfwQXBUXrhGGNEUGIMHYf-UXz7c';
 
@@ -29,6 +30,7 @@ class App extends Component {
     return (
       <div className="App">
         <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
       </div>
     );
   }
