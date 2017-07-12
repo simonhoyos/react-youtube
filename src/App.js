2f8140b622b0bacc7ef4ac37ca222aec92e701ca
shmesa22 <shmesa23@gmail.com>
2017-07-12 15:20:15 -0500

    Implement fucntionality search videos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import YTSSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBkYZoFpfwQXBUXrhGGNEUGIMHYf-UXz7c';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0];
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-
-      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+      </div>
     );
   }
 }
