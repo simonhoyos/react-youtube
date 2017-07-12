c8ce437ea731864168c6c089d321c381adefbb98
shmesa22 <shmesa23@gmail.com>
2017-07-12 15:56:17 -0500

    Fix video functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import YTSSearch from 'youtube-api-search';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBkYZoFpfwQXBUXrhGGNEUGIMHYf-UXz7c';
@@ -19,7 +20,7 @@ class App extends Component {
   }
 
   videoSearch(term) {
-    YTSSearch({key: API_KEY, term: term}, (videos) => {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos,
         selectedVideo: videos[0],
@@ -31,9 +32,13 @@ class App extends Component {
     const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 500);
 
     return (
-      <div className="App">
+      <div>
         <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={ this.state.videos } 
+        />
       </div>
     );
   }
