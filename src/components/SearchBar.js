9c643101a3acc29b6d60afffda8b22c73c9f9d87
Simon Hoyos <shmesa23@gmail.com>
2019-02-26 14:52:30 -0500

    Change npm for yarn

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-  
-    this.state = {
-      term: '',
-    };
-  }
+  state = {
+    term: '',
+  };
 
   render() {
     return(
@@ -20,10 +16,13 @@ class SearchBar extends Component {
     );
   }
 
-  onInputChange(term) {
+  onInputChange = (e) => {
+    const term = e.target.value;
+
     this.setState({
       term,
-    })
+    });
+
     this.props.onSearchTermChange(term);
   }
 }
